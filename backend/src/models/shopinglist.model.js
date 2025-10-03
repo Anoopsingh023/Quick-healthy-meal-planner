c50cdf6e0609d845e1174f90969c5165081f315d
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-10-04 00:21:26 +0530

    Refactor shopping list model to use ES6 import syntax; clean up user route file by removing commented-out code

--- backend/src/models/shopinglist.model.js
+++ backend/src/models/shopinglist.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const shoppingListSchema = new mongoose.Schema({
   userId: {
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,16 +1,3 @@
-// import { Router } from "express";
-// import {
-//     registerUser,
-//     loginUser
-// } from "../controllers/user.controller.js";
-
-// const router = Router()
-
-// router.route("/register").post(upload.single("avatar"),registerUser)
-// router.route("/login").post(loginUser);
-
-// export default router;
-
 import express from "express";
 import {
   registerUser,
