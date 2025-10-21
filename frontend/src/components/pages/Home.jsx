e5e6f914e49e250fce641690ae0a1cdf787a4c83
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-10-21 23:47:04 +0530

    feat: add saved recipes functionality and integrate calorie image in Home component

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import recipeImg from "../../assets/recipe.jpg";
 import useRecipe from "../../hooks/useRecipe";
+import calories from "../../assets/Calories.png";
+import { Timer } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const items = ["veg", "Egg", "Potato", "Tomato"];
-  const {recipe} = useRecipe()
+  const { recipe, savedRecipes } = useRecipe();
+  const navigate = useNavigate()
+  // console.log("Home saved recipes", savedRecipes);
+  const handleRecipe = (recipeId)=>{
+    navigate(`/dashboard/${recipeId}`)
+  }
+
   return (
     <div className="flex flex-col px-10">
       <div className="flex flex-row justify-between">
@@ -26,18 +35,56 @@ const Home = () => {
             ))}
           </div>
         </div>
-        <div className="w-96 border p-3 flex flex-col gap-2 rounded-2xl">
-          <h2 className="text-3xl font-semibold">Recipe Title</h2>
+
+        {/* Random Recipes */}
+        <div className=" border p-3 flex flex-col gap-2 rounded-2xl">
+          <h2 onClick={()=>handleRecipe(recipe?.data._id)} className="text-3xl font-semibold cursor-pointer">{recipe?.data?.title}</h2>
           <div className="flex flex-row gap-5">
-            <img className="w-30 h-30 rounded-2xl" src={recipeImg} alt="img" />
+            <img onClick={()=>handleRecipe(recipe?.data._id)} className="w-30 h-30 rounded-2xl cursor-pointer" src={recipeImg} alt="img" />
             <div className="flex flex-wrap gap-2">
-              {items.map((item) => (
-                <div key={item} className=" p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full">
-                  {item}
-                </div>
-              ))}
+              <p className=" p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full">
+                {recipe?.data?.metadata.dietType}
+              </p>
+              <p className=" p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full">
+                {recipe?.data?.metadata.cuisine}
+              </p>
+              <p className=" p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full">
+                {recipe?.data?.metadata.difficulty}
+              </p>
             </div>
           </div>
+          <div className="flex flex-row gap-2">
+            <p className=" p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full flex flex-row">
+              <Timer size={20} />
+              {recipe?.data?.metadata.cookingTime} min
+            </p>
+            <p className=" p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full flex flex-row">
+              <img className="h-5 w-5" src={calories} alt="" />
+              {Math.trunc(recipe?.data?.metadata.calories)} kcal
+            </p>
+            <p className=" p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full flex flex-row items-center">
+              Rs. {recipe?.data?.metadata.costEstimate}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <h2 className="text-3xl font-medium">Browse Recipe</h2>
+        <div className="flex flex-row gap-4">
+          {savedRecipes?.data.map((savedRecipe) => (
+            <div
+              key={savedRecipe._id}
+              className="flex flex-col gap-2 p-4 border rounded-2xl"
+            >
+              <img src={recipeImg} alt="" />
+              <h2>{savedRecipe.title}</h2>
+              <div className="flex flex-row gap-2">
+              <p className="p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full ">{savedRecipe.metadata.cookingTime}</p>
+              <p className="p-1 px-2 h-8 text-sm bg-[#b7b2b2] rounded-full">{savedRecipe.metadata.cuisine}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- frontend/src/hooks/useRecipe.js
+++ frontend/src/hooks/useRecipe.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios"
 import { base_url } from "../utils/constant";
 
-const useRecipe = ()=>{
+const useRecipe = (recipeId)=>{
     const [recipe, setRecipe] = useState()
+    const [savedRecipes, setSavedRecipes]= useState()
+    const [recipeById, setRecipeById] = useState()
 
     const getRandomRecipe = async()=>{
         try {
@@ -22,9 +24,39 @@ const useRecipe = ()=>{
         }
     }
 
+    const getSavedRecipes = async()=>{
+        try {
+            const res = await axios.get(`${base_url}/users/me/saved-recipes`,{
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                }
+            })
+            console.log("Saved Recipe",res.data)
+            setSavedRecipes(res.data||[])
+        } catch (error) {
+            console.log("Error in saved recipe",error)
+        }
+    }
+
+    const getRecipeById = async()=>{
+        try {
+            const res = await axios.get(`${base_url}/recipes/re/${recipeId}`,{
+                headers:{
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                }
+            })
+            console.log("Recipe by id",res.data)
+            setRecipeById(res.data || [])
+        } catch (error) {
+            console.log("Error in recipe by id",error)
+        }
+    }
+
     useEffect(()=>{
         getRandomRecipe()
+        getSavedRecipes()
+        getRecipeById()
     },[])
-    return {recipe}
+    return {recipe, savedRecipes, recipeById}
 }
 export default useRecipe
