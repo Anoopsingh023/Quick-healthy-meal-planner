b3b6a65a63fc34ff02e20edb4d142d3ccedab0e0
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-11-04 02:08:14 +0530

    FIx: create index file to export from one file follow industry standard

---
+++ frontend/src/shared/index.js
@@ -0,0 +1,22 @@
+import BackButton from "./BackButton";
+import BadgesCard from "./BadgesCard";
+import Ingredients from "./Ingredients";
+import RecipeCard from "./RecipeCard";
+import Search from "./Search";
+import ShoppingItem from "./ShoppingItem";
+import SliderCard from "./SliderCard";
+import { Tag, TimeTag, PriceTag, CalorieTag } from "./Tag";
+
+export {
+  BackButton,
+  BadgesCard,
+  Ingredients,
+  RecipeCard,
+  Search,
+  ShoppingItem,
+  SliderCard,
+  Tag,
+  TimeTag,
+  PriceTag,
+  CalorieTag,
+};
