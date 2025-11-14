26489d5c268851f3589ef5bff98cf28e02331402
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-11-15 01:08:17 +0530

    Feat: add index files for pages and user components to streamline exports

---
+++ frontend/src/components/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import Navbar from "./Navbar";
+import RecipeDetail from "./RecipeDetail";
+import Sidebar from "./Sidebar";
+import ShoppingList from "./ShoppingList";
+import SavedRecipe from "./SavedRecipe";
+import SearchResults from "./SearchResults";
+import ProfilePage from "./ProfilePage";
+
+export {
+  Home,
+  Navbar,
+  RecipeDetail,
+  Sidebar,
+  ShoppingList,
+  SavedRecipe,
+  SearchResults,
+  ProfilePage
+};
---
+++ frontend/src/components/user/index.js
@@ -0,0 +1,5 @@
+import AllergiesCard from "./AllergiesCard";
+import PreferencesCard from "./PreferencesCard";
+import ProfileHeader from "./ProfileHeader";
+
+export {AllergiesCard, PreferencesCard, ProfileHeader }
