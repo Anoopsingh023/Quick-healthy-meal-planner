77a31a732ce604e7219fe80e8652057ea77c88a2
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2026-01-13 01:05:47 +0530

    Feat: update base_url to include API version for production server

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
 // export const base_url = "http://localhost:4000/api/v1"
-export const base_url = "https://quick-healthy-meal-planner.onrender.com"
+export const base_url = "https://quick-healthy-meal-planner.onrender.com/api/v1"
