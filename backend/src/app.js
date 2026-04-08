34afe93c38ac1e4277454b2c69fc7a68b1ccc67b
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2026-04-08 17:57:12 +0530

    feat: update CORS configuration to use environment variable for origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,8 @@ const app = express()
 
 
 app.use(cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
+    // origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(express.json({limit: "16kb"}))
